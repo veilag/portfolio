954ce825fb738db189512503285afad8e891e48d
veilag <web.veilag@gmail.com>
2024-10-15 17:11:12 +0300

    📝 [Update] Title has sticky position, while about is revealed

--- www/src/components/Content.tsx
+++ www/src/components/Content.tsx
@@ -7,15 +7,17 @@ const Content = () => {
   const [isAboutRevealed, setAboutRevealed] = useState<boolean>(false)
 
   return (
-    <div className="h-[calc(100vh-8rem)] px-10 w-full max-w-[1420px] m-auto">
+    <div className="min-h-[calc(100vh-8rem)] px-10 w-full max-w-[1420px] m-auto">
+      {isAboutRevealed && <div className="fixed left-0 z-10 bottom-0 w-full h-28 fade pointer-events-none"></div>}
       <div className="flex h-full justify-between top-2 gap-32">
-        <div className="flex flex-col gap-5 top-10 justify-between pb-10">
+        <div className="flex flex-col gap-5 top-10 justify-start pb-10">
           <div className="flex gap-20">
             <ContentBlock title="Инструменты / Библиотеки" list={toolsAndLibs}/>
             <ContentBlock title="Последние работы" list={works}/>
           </div>
           
           <motion.div
+            className="sticky top-8"
             transition={{
               duration: 1,
               type: "spring",
@@ -55,7 +57,19 @@ const Content = () => {
         }}
         className="flex-1 max-w-[800px]">
           <p className="text-neutral-400 top-4">О себе</p>
-          <div className={`relative mt-2 ${!isAboutRevealed && 'h-96'} overflow-clip pb-10 text-lg no-scrollbar`}>
+          <motion.div
+            initial={{
+              height: '24rem'
+            }}
+            animate={{
+              height: isAboutRevealed ? '100%' : '24rem'
+            }}
+            transition={{
+              duration: 1.5,
+              ease: "easeOut"
+            }}
+
+            className={`relative mt-2 overflow-clip pb-10 text-lg no-scrollbar`}>
             {new Array(16).fill("").map(() => (
               <p>
                 Lorem ipsum dolor sit amet consectetur. Eget urna faucibus nibh rutrum eu dictum mauris in. Dui volutpat
@@ -65,8 +79,8 @@ const Content = () => {
               </p>
             ))}
             
-            {!isAboutRevealed  && <div className="text-sm absolute bottom-0 w-full h-20 fade pointer-events-none"></div>}
-          </div>
+            {!isAboutRevealed  && <div className="absolute bottom-0 w-full h-20 fade pointer-events-none"></div>}
+          </motion.div>
           <motion.button
             transition={{
               duration: 1,
@@ -84,7 +98,10 @@ const Content = () => {
               filter: "blur(0px)",
             }}
             onClick={() => setAboutRevealed(true)} 
-            className={`bg-white ${isAboutRevealed ? 'hidden' : 'block'} font-medium rounded-full px-5 py-1.5 mt-4 text-black`}>Раскрыть</motion.button>
+            className={`bg-white ${isAboutRevealed ? 'hidden' : 'block'} font-medium rounded-full px-5 py-1.5 mt-4 text-black`}
+          >
+            Читать
+          </motion.button>
         </motion.div>
       </div>
     </div>
